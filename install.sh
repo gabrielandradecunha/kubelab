0cc8d698a67d28ec0c5e3b716081c2825708d79f
Gabriel Andrade <gandradecortez50@gmail.com>
2025-01-12 19:24:45 -0400

    change name

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if ! command -v git --version > /dev/null; then
     exit 1
 fi
 
-git clone https://github.com/andradesysadmin/homelab
+git clone https://github.com/homemlinux/homelab
 cd homelab
 sudo chmod +x start.sh
 sudo ./start.sh
